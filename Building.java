0ba81835fccb242cfef6a3380537fbe85e8ed3c2
Larry Denys <lddenys@gmail.com>
2016-04-06 11:42:56 -0600

    Start of facade for simulation plus bulding.  Not much to add.  :(

---
+++ Building.java
@@ -0,0 +1,22 @@
+package foo;
+
+public class Building {
+   private int minFloor;
+   private int maxFloor;
+
+   public Building( int min, int max ) {
+      minFloor = min;
+      maxFloor = max;
+   }
+
+   /** @return True if okay for elevator to go to floor #.
+    *          False if out of bounds, raging fire, whatever
+    */
+   protected boolean floorOkay( int floor ) {
+      return ((floor >= minFloor) && (floor <= maxFloor));
+      
+      //FIXME: Maybe we should throw an exception for fire, terrorists...
+   } 
+}
+
+
